7c864c322b0019990a35838985d5678bb2a7dea5
mengyx-work <ymeng.ucla@gmail.com>
2016-08-19 15:08:39 -0700

    file to generate locale related features

---
+++ event_gen_locale_features.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from scipy.sparse import csr_matrix, hstack
+
+data_path = '/home/ymm/data/talkingdata_data/'
+
+tmp_device_feature_file = 'events_time_fea_with_reg_fea_data.csv'
+device_feature_df = pd.read_csv(data_path + tmp_device_feature_file, index_col='device_id')
+print 'device_feature_df of time_fea and regular_fea are saved into:', device_feature_file
+
+events_labeled_file = 'events_labeled_data.csv'
+events_labeled = pd.read_csv(data_path + events_labeled_file, index_col='event_id')
+print 'events_labeled is loaded with shape:', events_labeled.shape 
+
+## separate the location columns
+location_events = events_labeled[['longitude', 'latitude', 'device_id']]
+location_events['approx_dist'] = location_events[['longitude', 'latitude']].apply(lambda x: np.sqrt(x[0]*x[0] + x[1]*x[1]), axis = 1)
+
+## generate location-related features from three raw features
+geo_fea_func_dict = {'max' : np.max, 'min' : np.min, 'mean' : np.mean}
+geo_fea_df = None
+for feature_name in ['longitude', 'latitude', 'approx_dist']:
+    ## derive features from single feature using functions from a dictionary called geo_fea_func_dict
+    tmp_df = agg_column_derive_fea(location_events, 'device_id', feature_name, geo_fea_func_dict)
+    ## concatenate all the features together
+    if geo_fea_df is None:
+        geo_fea_df = tmp_df
+    else:
+        geo_fea_df = pd.merge(geo_fea_df, tmp_df, how='outer', left_index=True, right_index=True)
+        
+## merge location feature to the device_feature df
+print 'location feature shape:', geo_fea_df.shape
+device_feature_df = pd.merge(device_feature_df, geo_fea_df, how='outer', left_index=True, right_index=True)
+print 'after merging with location feature shape:', device_feature_df.shape
+
+device_feature_file = 'devide_feature_data.csv'
+device_feature_df.to_csv(data_path + device_feature_file)
+
+
