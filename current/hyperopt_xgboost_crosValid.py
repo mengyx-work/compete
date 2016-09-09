1245135d8f85a7e85614e8a92c059f362e5a9f36
mengyx-work <ymeng.ucla@gmail.com>
2016-09-09 22:36:35 +0000

    default to 2 fold

--- current/hyperopt_xgboost_crosValid.py
+++ current/hyperopt_xgboost_crosValid.py
@@ -20,7 +20,7 @@ from utils.utils_functions import utils_functions
 # parameter can be
 # a. three element tuple
 # b. a list of discrete values
-tuning_params = {'eta':(0.001, 0.01, 0.0005), 'num_round':(1000, 10000, 500)}
+tuning_params = {'eta':(0.005, 0.01, 0.0005), 'num_round':(1000, 8000, 500)}
 
 # constant parameters as a dictionary
 # avant standard xgboost params
@@ -36,7 +36,7 @@ params['eval_metric']              = 'auc'
 params["seed"]                     = 100
 
 params["val"]                      = False ## whether create a valid dataset
-params["early_stopping_ratio"]     = 0.2
+#params["early_stopping_ratio"]     = 0.2
 
 dep_var_name = 'Response'
 data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_processed_data'
@@ -57,6 +57,6 @@ hyperopt = hyperopt_xgboost(data, dep_var_name,
                             const_params  = params,
                             data_filename = 'bosch_xgboost_hyperopt_results.csv',
                             crosValid_mode = True,
-                            fold_num = 3,
+                            fold_num = 2,
                             max_opt_count = 400)
 hyperopt.hyperopt_run()
