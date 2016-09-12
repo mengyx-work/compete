8ef3ce4c96e51736128708797327414d26b485a4
mengyx-work <ymeng.ucla@gmail.com>
2016-09-12 20:07:25 +0000

    update grid search scripts

--- current/gridSearch_RF.py
+++ current/gridSearch_RF.py
@@ -7,8 +7,35 @@ sys.path.append('/home/ymm/kaggle/xgboost_hyperopt')
 import utils
 from utils.models import RandomForestModel, ExtraTreeModel
 from utils.validation_tools import score_MCC, grid_search_cross_validate_model, list_const_params
+from utils.bosch_functions import load_processed_bosch_data
+
+## 15 bins data
+#project_path = '/mnt/home/ymm/kaggle/compete/current'
+#data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
+#yaml_file = 'bosch_processed_data_dict.yml'
+
+## 6 bins data
+project_path = '/home/ymm/kaggle/compete/current/model_6_bins_data'
+data_path = '/home/ymm/kaggle/bosch_data/bosch_complete_processed_6_bins_data'
+data_yaml_file = 'complete_subset_data_6_bins_dict.yml'
+data_index= '0'
+train = load_processed_bosch_data(data_path, project_path, data_yaml_file, data_index=data_index)
+
+const_param_dict = {'random_state' : 0, 'criterion' : 'entropy', 'n_jobs' : -1}
+#const_param_dict = {'criterion' : 'entropy', 'n_jobs' : -1, 'n_estimators' : 500}
+const_param_dict = list_const_params(const_param_dict)
+#tuning_param_dict = {'random_state': range(0, 10), 'max_depth':[4, 7]}
+tuning_param_dict = {'n_estimators': [500, 1000, 1500, 2000, 2500], 'max_depth':range(3, 8)}
+
+param_dict = const_param_dict.copy()
+param_dict.update(tuning_param_dict)
+
+cv_fold_num = 2
+result_file_name = 'RandomForest_{}_{}_fold_GridSearch_Results.csv'.format(const_param_dict['criterion'][0], cv_fold_num)
+grid_search_cross_validate_model(train, dep_var_name, RandomForestModel, score_MCC, param_dict, cv_fold_num, result_file=result_file_name)
 
 
+'''
 project_path = '/mnt/home/ymm/kaggle/compete/current'
 data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
 
@@ -31,4 +58,4 @@ param_dict = const_param_dict.copy()
 param_dict.update(tuning_param_dict)
 
 grid_search_cross_validate_model(train, dep_var_name, RandomForestModel, score_MCC, param_dict, 2, result_file='RF_gini_3_depth_GridSearch_2fold_Results.csv')
-
+'''
