56b670ba95d312ad31b2046b1d11fafd929ce985
mengyx-work <ymeng.ucla@gmail.com>
2016-09-14 00:07:09 +0000

    update xgb model

--- current/single_xgb_model.py
+++ current/single_xgb_model.py
@@ -1,63 +1,60 @@
 import pandas as pd
 import numpy as np
 import os, sys, time
-import yaml
-from sklearn.metrics import matthews_corrcoef
 
-## linux path
 sys.path.append('/home/ymm/kaggle/xgboost_hyperopt')
-## Mac path
-#sys.path.append('/Users/ymm/Google_Drive/dev/xgboost_hyperopt')
 import utils
 from utils.wrapped_xgboost import xgboost_classifier
-from utils.validation_tools import cross_validate_model, score_MCC
+from utils.models import RandomForestModel, ExtraTreeModel, XgboostModel
+from utils.validation_tools import score_MCC, create_validation_index
 from utils.bosch_functions import load_processed_bosch_data
 
-
-
-dep_var_name = 'Response'
-
+dep_var_name     = 'Response'
 '''
-## old way to laod data based on the yaml file content
-project_path = '/home/ymm/kaggle/compete/current'
-data_path = '/home/ymm/kaggle/bosch_data/bosch_processed_data'
-
-yaml_file = 'bosch_processed_data_dict.yml'
-with open(os.path.join(project_path, yaml_file), 'r') as yml_stream:
-    data_dict = yaml.load(yml_stream)
-
-data_index = '0'
-data_file = os.path.join(data_path, data_dict[data_index]['train_file'])
-print 'loading data from ', data_file
-'''
-
-'''
-## load data in local machine
-data_file = '/Users/ymm/Downloads/kaggle/bosch/processed_totBins_16_bin_0_train.csv'
-train = pd.read_csv(data_file, index_col='Id')
-'''
-
-## load training data
+## smaller data
 project_yml_path = '/mnt/home/ymm/kaggle/compete/current'
-data_path = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
-data_yaml_file = 'bosch_processed_data_dict.yml'
+data_path        = '/mnt/home/ymm/kaggle/bosch_data/bosch_complete_processed_data'
+data_yaml_file   = 'bosch_processed_data_dict.yml'
+'''
+
+project_yml_path = '/home/ymm/kaggle/compete/current/model_6_bins_data'
+data_path = '/home/ymm/kaggle/bosch_data/bosch_complete_processed_6_bins_data'
+data_yaml_file = 'complete_subset_data_6_bins_dict.yml'
+#'''
+
 train = load_processed_bosch_data(data_path, project_yml_path, data_yaml_file, data_index='0')
+print train.shape
+train_index,valid_index = create_validation_index(train, 0.4, dep_var_name, True)  
+valid_data  = train.ix[valid_index]
+train       = train.ix[train_index]
+valid_label = valid_data[dep_var_name]
 
 params = {}
-params["eta"]                      = 0.01
-params["subsample"]                = 0.8
-params["colsample_bytree"]         = 0.8
-params["num_round"]                = 500
-params["max_depth"]                = 5
+params["eta"]                      = 0.001
+params["subsample"]                = 1.0
+params["colsample_bytree"]         = 1.0
+params["num_round"]                = 4500
+params["max_depth"]                = 7
 params["gamma"]                    = 0
 params["metrics"]                  = 'auc'
 params['eval_metric']              = 'auc'
 params["seed"]                     = 100
+params['scale_pos_weight']         = 10.
+## whether to use weights
+params['use_weights']              = True
 
 params["val"]                      = False
-params["early_stopping_ratio"]     = 0.2
+#params["early_stopping_ratio"]     = 0.2
 
+#model = XgboostModel(params)
+model = xgboost_classifier(label_name = dep_var_name, params = params, model_file='large_data_bin_0_xgb_model')
+model.fit(train, dep_var_name)
+valid_result = model.predict(valid_data)
+print 'the MCC score:', score_MCC(valid_label, valid_result)
+
+'''
+## use the cross_validate_fit function
 xgb_clf = xgboost_classifier(label_name = dep_var_name, params = params)
 results = xgb_clf.cross_validate_fit(score_MCC, train, n_folds=2)
-print results
-
+print 'results from cross validation:', results
+'''
