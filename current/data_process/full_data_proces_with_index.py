e0297f84f88857760e5e133e011244a1ebc8bc3a
mengyx-work <ymeng.ucla@gmail.com>
2016-10-19 21:17:02 +0000

    update the script to process the categorical and station features

--- current/data_process/full_data_proces_with_index.py
+++ current/data_process/full_data_proces_with_index.py
@@ -15,6 +15,10 @@ from utils.models import CombinedModel
 from utils.data_munge import remove_single_value_columns
 from utils.feature_engineering import NumericalFeatureEngineering, getRelativeTimeColumns, BasicDate_FeatureEngineering
 from utils.feature_engineering import getTimeChangeColumns, getTimeSteps, build_IndexFeatures
+## station-related feature functions
+from utils.feature_engineering import build_column_dict, build_station_features, build_station_index_features 
+## categorical fetures related
+from utils.feature_engineering import BasicCat_FeatureEngineering, encode_categorical_by_dep_var
 
 data_path = '/home/ymm/bosch/'
 
@@ -33,15 +37,68 @@ dep_var_name = 'Response'
 
 start_time = time.time()
 ## loading the data by using the skipped_row_num list
-train_num = pd.read_csv(join(data_path, train_num_file),    index_col='Id')
-train_dat = pd.read_csv(join(data_path, train_date_file),   index_col='Id')
-test_num = pd.read_csv(join(data_path, test_num_file),      index_col='Id')
-test_dat = pd.read_csv(join(data_path, test_date_file),     index_col='Id')
+'''
+train_num   = pd.read_csv(join(data_path, train_num_file),    index_col='Id')
+train_dat   = pd.read_csv(join(data_path, train_date_file),   index_col='Id')
+train_cat   = pd.read_csv(join(data_path, train_cat_file),   index_col='Id')
+test_num    = pd.read_csv(join(data_path, test_num_file),      index_col='Id')
+test_dat    = pd.read_csv(join(data_path, test_date_file),     index_col='Id')
+test_cat    = pd.read_csv(join(data_path, test_date_file),     index_col='Id')
+'''
+num_rows = 50000
+train_num = pd.read_csv(join(data_path, train_num_file),    index_col='Id', nrows=num_rows)
+train_dat = pd.read_csv(join(data_path, train_date_file),   index_col='Id', nrows=num_rows)
+train_cat = pd.read_csv(join(data_path, train_cat_file),    index_col='Id', nrows=num_rows)
+test_num = pd.read_csv(join(data_path, test_num_file),      index_col='Id', nrows=num_rows)
+test_dat = pd.read_csv(join(data_path, test_date_file),     index_col='Id', nrows=num_rows)
+test_cat = pd.read_csv(join(data_path, test_cat_file),      index_col='Id', nrows=num_rows)
+
 
 print 'finish loading date using {} seconds'.format(round(time.time() - start_time, 0))
 
 remove_single_value_columns(train_num, 'Response', test=test_num)
 remove_single_value_columns(train_dat, test=test_dat)
+
+#### station-based feature engineering works
+## create column dictionaries
+dat_col_dict, dat_line_dict = build_column_dict(dat_columns)
+num_col_dict, num_line_dict = build_column_dict(num_columns)
+dat_col_dict.update(dat_line_dict)
+num_col_dict.update(num_line_dict)
+
+start_time = time.time()
+train_dat_stations = build_station_features(train_dat, dat_col_dict, 'dat')
+test_dat_stations = build_station_features(test_dat, dat_col_dict, 'dat')
+
+train_num_stations = build_station_features(train_num, num_col_dict, 'num')
+test_num_stations = build_station_features(test_num, num_col_dict, 'num')
+
+num_station_index = build_station_index_features(train_num_stations, test_num_stations)
+dat_station_index = build_station_index_features(train_dat_stations, test_dat_stations)
+
+combined_train_station_num = pd.concat([train_num_stations, num_station_index.ix[train_num_stations.index]], axis=1)
+combined_train_station_dat = pd.concat([train_dat_stations, dat_station_index.ix[train_dat_stations.index]], axis=1)
+
+combined_test_station_num = pd.concat([test_num_stations, num_station_index.ix[train_num_stations.index]], axis=1)
+combined_test_station_dat = pd.concat([test_dat_stations, dat_station_index.ix[train_dat_stations.index]], axis=1)
+
+print 'finish feature engineering staiont features using {} minutes'.format(round((time.time() - start_time)/60, 2))
+
+
+#### categorical features FE
+start_time = time.time()
+
+train_cat['Response'] = train_num['Response']
+encode_categorical_by_dep_var(train_cat, test_cat)
+train_cat.drop('Response', axis=1, inplace=True)
+
+train_cat_Basics = BasicCat_FeatureEngineering(train_cat)
+test_cat_Basics  = BasicCat_FeatureEngineering(train_cat)
+
+combined_train_cat = pd.concat([train_cat, train_cat_Basics], axis=1)
+combined_test_cat  = pd.concat([test_cat, test_cat_Basics], axis=1)  
+print 'finish generating categorical features using {} seconds'.format(round(time.time() - start_time, 0))
+
 
 #### numerical feature engineering work
 start_time = time.time()
@@ -52,6 +109,9 @@ combined_train_num = pd.concat([train_num, train_num_Basics], axis=1)
 combined_test_num  = pd.concat([test_num, test_num_Basics], axis=1)                                                                            
 print 'finish generating numercail features and new dataset using {} minutes'.format(round((time.time()-start_time)/60, 2))
 print 'combined train numerical feature shape: {}, combined test numerical features shape: {}'.format(combined_train_num.shape, combined_test_num.shape)
+
+
+#### date features engineering works
 
 ## basic features from tmp_train_dat
 start_time = time.time()
@@ -127,8 +187,9 @@ train_test_datIndex_features = build_IndexFeatures(combined_train_dat, combined_
 print 'finish index feature generation using {} minutes'.format(round((time.time() - start_time)/60, 2))
 
 start_time = time.time()
-combined_train = pd.concat([combined_train_num, combined_train_dat, train_test_datIndex_features.ix[combined_train_num.index, :]], axis=1)
-combined_test  = pd.concat([combined_test_num,  combined_test_dat,  train_test_datIndex_features.ix[combined_test_num.index, :]], axis=1)
-combined_train.to_csv('FE_raw_train_num_dat_data.csv') 
-combined_test.to_csv('FE_raw_test_num_dat_data.csv') 
+combined_train = pd.concat([combined_train_station_dat, combined_train_station_num, combined_train_cat, combined_train_num, combined_train_dat, train_test_datIndex_features.ix[combined_train_num.index, :]], axis=1)
+combined_test  = pd.concat([combined_test_station_dat, combined_test_station_num, combined_test_cat,  combined_test_num,  combined_test_dat,  train_test_datIndex_features.ix[combined_test_num.index, :]], axis=1)
+
+combined_train.to_csv('FE_raw_train_full_index.csv') 
+combined_test.to_csv('FE_raw_test_full_index.csv') 
 print 'finish writing datea into csv files using {} minutes'.format(round((time.time() - start_time)/60, 2))
